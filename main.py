a01cf4bea11b325dc31a12f0abd461cc685395a1
bundulappp <bundulappp@gmail.com>
2024-10-06 17:47:10 +0200

    show player on the screen

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+from constants import *
+from player import *
+
+def main():
+    print('Start asteroids!')
+
+
+if __name__ == "__main__":
+    #pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    running = True
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
+    #Game loop
+    while running:
+        # event for exit request
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill('black')
+        player.draw(screen)
+         # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        # limits FPS to 60
+        # dt is delta time in seconds since last frame, used for framerate-
+        # independent physics.
+        dt = clock.tick(60) / 1000
+        
+    pygame.quit()
+            
